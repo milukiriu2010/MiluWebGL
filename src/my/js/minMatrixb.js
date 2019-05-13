cb254ffdf8ba130cc3da6bef946c2945734f8a86
milu(Mitsuharu Kiriu) <milu.kiriu2010@gmail.com>
2019-05-13 09:08:18 +0900

    cube_transform_02

--- src/my/js/minMatrixb.js
+++ src/my/js/minMatrixb.js
@@ -15,6 +15,16 @@ function matIV(){
 		dest[12] = 0; dest[13] = 0; dest[14] = 0; dest[15] = 1;
 		return dest;
 	};
+	
+	this.copy = function(src) {
+		var dst = new Float32Array(16);
+		dst[0]  = src[0];  dst[1]  = src[1];  dst[2]  = src[2];  dst[3]  = src[3];
+		dst[4]  = src[4];  dst[5]  = src[5];  dst[6]  = src[6];  dst[7]  = src[7];
+		dst[8]  = src[8];  dst[9]  = src[9];  dst[10] = src[10]; dst[11] = src[11];
+		dst[12] = src[12]; dst[13] = src[13]; dst[14] = src[14]; dst[15] = src[15];
+		return dst;
+	}
+	
 	this.multiply = function(mat1, mat2, dest){
 		var a = mat1[0],  b = mat1[1],  c = mat1[2],  d = mat1[3],
 			e = mat1[4],  f = mat1[5],  g = mat1[6],  h = mat1[7],
